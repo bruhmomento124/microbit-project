00038a54f3e51c217541204e91cbe1a10f945158
playtouhou <87738387+bruhmomento124@users.noreply.github.com>
2022-11-25 10:56:22 +1000

    some fixes

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ the microbit sends the data depending on what the function is, and the python pr
 this explanation isnt the greatest, but you can find more in depth explanations by reading the commends throughout the code
 """
 
-import serial, random
+import serial, random, time
 # this serial port can change, microbit is weird
 ser = serial.Serial('COM3', 115200) # serial port and baud rate
-
+score = 0 # score variable
 try: # if score.txt exists, print "High Score: " and the score
     with open("score.txt", "r") as f:
         print("High Score: " + f.read())
@@ -26,13 +26,14 @@ def game():
     options = ["a", "ab", "b", "shake"] # available functions from the microbit
     choice = random.choice(options) # making a variable for easier use
     print(choice)
-    score = 0 # score variable
     i = True
     while i == True:
         # choices = [""] # unused
         data = ser.readline().decode('utf-8').rstrip() # reads serial data and strips excess formatting
         if data == choice: # this code could look wayyyy better, but its rushed
             print("correct choice" + "\n" * 4) # the \n * 4 is to separate for the next game
+            global score
+            score += 1
             i = False
             score += 1 # self explanatory
         else:
@@ -46,6 +47,9 @@ def game():
             except FileNotFoundError:
                 with open("score.txt", "w") as f:
                     f.write(str(score))
+            print("final score: " + str(score))
+            time.sleep(0.5)
+            exit()
 
 while True:
     game()
